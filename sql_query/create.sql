b6d062e1e092e3b7a9b75d34fd47af06e1d30d98
Sara Mordehayov <saramor325@gmail.com>
2020-11-16 20:26:34 +0200

    queries

--- sql_query/create.sql
+++ sql_query/create.sql
@@ -1,6 +1,6 @@
--- create database healthy_app;
+ create database healthy_app;
 
-USE healthy_app; 
+-- USE healthy_app; 
 
 -- drop TABLE user_day;
 -- drop TABLE user;
@@ -9,9 +9,11 @@ CREATE TABLE user(
     user_name varchar(50) not null  PRIMARY KEY,
     name varchar(50),
     birth_date date,
+    weight float,
+    height float,  
     max_calories float,
     max_fat float,
-    max_sugar float,
+    max_carb float,
     max_protein float,
     current_state varchar(50)
     );
@@ -22,11 +24,12 @@ CREATE TABLE user_day(
     date_of_day date,
     calories float,
     fat float,
-    sugar float,
+    carb float,
     protein float,
 
     PRIMARY KEY (user_name, date_of_day),
 
     FOREIGN KEY(user_name) REFERENCES user(user_name)
 )
+
 
--- users_model.py
+++ users_model.py
@@ -1,47 +1,79 @@
 import pymysql
+import datetime
+from config import DB_HOST, DB_USER, DB_PASSWORD
 
 connection = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="",
-    db="store",
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    db="healthy_app",
     charset="utf8",
     cursorclass=pymysql.cursors.DictCursor
 )
 
 
 def do_query(query_str):
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(query_str)
-            result = cursor.fetchall()
-            return result
-    except Exception as x:
-        return None
+    with connection.cursor() as cursor:
+        cursor.execute(query_str)
+        result = cursor.fetchall()
+        return result
+
     
 
 def do_query_with_change(query_str):
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute(query_str)
-            connection.commit()
-            return True
-    except Exception as x:
-        return False
+    with connection.cursor() as cursor:
+        cursor.execute(query_str)
+        connection.commit()
+        return True
 
-def add_user(user_name_, name_, birth_date_, max_calories_, max_fat_, max_sugar_, max_protein_, current_state_ = "main"):
-    pass
+def add_user(user_name_, name_, birth_date_, weight_, height_, max_calories_, max_fat_, max_crab_, max_protein_, current_state_ = "main"):
+    date_for_query = birth_date_.strftime("%Y-%m-%d")
+    query = "INSERT INTO user VALUES('{}', '{}', '{}', {}, {}, {}, {}, {}, {}, '{}')".format(user_name_, name_, date_for_query, weight_, height_, max_calories_, max_fat_, max_crab_, max_protein_, current_state_)
+    succ = do_query_with_change(query) 
+    if succ:
+        return True
+    return False
+
+def is_exist_user(user_name):
+    res = False
+    query = "SELECT * FROM user as u where u.user_name = '{}'".format(user_name)
+    items = do_query(query)
+    if items:
+        res = True
+    return res
 
 def get_user(user_name):
-    pass
+    res = None
+    query = "SELECT * FROM user as u where u.user_name = '{}'".format(user_name)
+    items = do_query(query)
+    if items:
+        res = items[0]
+    return res
 
 def is_exist_user_day(user_name, req_date):
-    pass
+    res = False
+    date_for_query = req_date.strftime("%Y-%m-%d")
+    query = '''SELECT * 
+    FROM user as u join user_day as ud 
+    on u.user_name = ud.user_name
+    where u.user_name = '{}' and ud.date_of_day = '{}' '''.format(user_name, date_for_query)
+    items = do_query(query)
+    if items:
+        res = True
+    return res
 
 def add_user_day(user_name, req_date):
-    pass
+    date_for_query = req_date.strftime("%Y-%m-%d")
+    query = "INSERT INTO user_day VALUES('{}', '{}', 0, 0, 0, 0)".format(user_name, date_for_query)
+    succ = do_query_with_change(query) 
+    if succ:
+        return True
+    return False
 
 def get_user_day(user_name, req_date):
-    pass
-
-
+    res = None
+    query = "SELECT * FROM user_day as ud where ud.user_name = '{}' and ud.date_of_day = '{}'".format(user_name, req_date)
+    items = do_query(query)
+    if items:
+        res = items[0]
+    return res
